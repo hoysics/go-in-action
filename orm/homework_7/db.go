bbb8badc2885eace6a288ca3c66baa6b781fc0fe
hoysics <18087814+hoysics@users.noreply.github.com>
2022-12-14 23:26:51 +0800

    feat(homework8):1.支持update语句和非0值更新

---
+++ orm/homework_7/db.go
@@ -0,0 +1,57 @@
+package homework_7
+
+import (
+	"database/sql"
+	valuer2 "github.com/hoysics/go-in-action/orm/homework_7/internal/valuer"
+	"github.com/hoysics/go-in-action/orm/homework_7/model"
+)
+
+type DBOption func(*DB)
+
+type DB struct {
+	r          model.Registry
+	db         *sql.DB
+	valCreator valuer2.Creator
+}
+
+func Open(driver string, dsn string, opts ...DBOption) (*DB, error) {
+	db, err := sql.Open(driver, dsn)
+	if err != nil {
+		return nil, err
+	}
+	return OpenDB(db, opts...)
+}
+
+func OpenDB(db *sql.DB, opts ...DBOption) (*DB, error) {
+	res := &DB{
+		r:          model.NewRegistry(),
+		db:         db,
+		valCreator: valuer2.NewUnsafeValue,
+	}
+	for _, opt := range opts {
+		opt(res)
+	}
+	return res, nil
+}
+
+func DBWithRegistry(r model.Registry) DBOption {
+	return func(db *DB) {
+		db.r = r
+	}
+}
+
+func DBUseReflectValuer() DBOption {
+	return func(db *DB) {
+		db.valCreator = valuer2.NewReflectValue
+	}
+}
+
+// MustNewDB 创建一个 DB，如果失败则会 panic
+// 我个人不太喜欢这种
+func MustNewDB(driver string, dsn string, opts ...DBOption) *DB {
+	db, err := Open(driver, dsn, opts...)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
